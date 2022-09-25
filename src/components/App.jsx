c59f8416543384dba51d29fc05f5fd6fb237019f
jephersonnn <ericjeffrey.sch@gmail.com>
2022-09-25 17:09:30 +0800

    Add Add and Delete Note function

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,15 +1,40 @@
-import React from "react";
+import React, {useState} from "react";
 import Header from "./Header";
 import Footer from "./Footer";
 import Note from "./Note";
 import CreateArea from "./CreateArea";
 
 function App() {
+
+  const [items, setItems] = useState([]); //main array of todo items
+ 
+
+  function newItem(item){
+    setItems((allTodo)=> {
+      return [...allTodo, item]
+    }) //the item being used here is from the newItem state from CreateArea.jsx
+    //spread operator being used to appened 'item' to the current list of items 'allTodo' 
+  }
+
+  function deleteItem(id){
+    setItems(items.filter((notes, index) => {return index !==id}))
+    //update items array with items.filter
+    //.filter is a .map function that filters items that satisfy the condition
+    //so the items being filtered here is the item with the 'id' passed as a prop from Note.jsx (actually from this file, items.map and passed as a prop to Note.jsx )
+    //'index !== id' basically means only return items without this id, technically deleting it
+  }
+
   return (
     <div>
       <Header />
-      <CreateArea />
-      <Note key={1} title="Note title" content="Note content" />
+      <CreateArea onAdd={newItem} /> 
+      {/* passing a function as a prop to CreateArea.jsx */}
+      {items.map((noteItem, index) => {
+        return (
+          <Note key={index} id={index} title={noteItem.title} content={noteItem.content} onDelete={deleteItem}/>
+        )
+        {/* passing key, id, title, content, and a function deleteItem as props for Note.jsx */}
+      })}
       <Footer />
     </div>
   );
--- src/components/CreateArea.jsx
+++ src/components/CreateArea.jsx
@@ -1,12 +1,35 @@
-import React from "react";
+import React, {useState} from "react";
 
-function CreateArea() {
+function CreateArea(props) {
+const [newItem, setItem] = useState({title: "", content: ""}) //array for todo items, temporary state with items to be passed later on
+
+  function handleChange(event){
+    const {value, name} = event.target; //destructuring event.target to avoid lengthy references
+
+    setItem(prevNote => {
+      return {
+        ...prevNote,
+        [name]: value //spread operator is used but '[name]' only pertains to the existing on the object on 'newItem'. so '...prevNote' pertains to the object containing 'title' and 'content'
+        //'[name]' may either be 'title' or 'content', and shall be updated with a new value 'value'
+      };
+    }); //setting current state with what is currently being entered
+  }
+
+  function submitNote(event){
+    props.onAdd(newItem); //pass the newItem being constantly set on the previous block of codes setItem
+    setItem({
+      title: "",
+      content: ""
+    }); //reset and clear the items
+    event.preventDefault(); //prevents the page from being refreshed on submit
+  }
+
   return (
     <div>
       <form>
-        <input name="title" placeholder="Title" />
-        <textarea name="content" placeholder="Take a note..." rows="3" />
-        <button>Add</button>
+        <input onChange={handleChange} name="title" placeholder="Title" value={newItem.title} />
+        <textarea onChange={handleChange} name="content" placeholder="Take a note..." rows="3" value={newItem.content} />
+        <button onClick={submitNote}>Add</button>
       </form>
     </div>
   );
